a8355e1753a92bb639ef9bbfef6ba11d6b9c3dbd
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-04-03 21:58:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 window.addEventListener("load", () => {
-  //start your code here
-});
+//   //start your code here
+  
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const numbers = "0123456789"
+  const symbols = "!#$%&()*+,-./:;<=>?@[\]^_{|}~"
+  
+  function onSubmit(event) {
+    event.preventDefault()
+    const input = document.getElementById('password-length')
+    let passwordLength = input.value
+    if(input.value < 4) {
+      passwordLength = 4
+    }
+    
+    //How to get the password length to have a max length?//
+
+    let startingString = ''
+  
+    for (i = 0; i < passwordLength; i++) {
+      if (startingString.length < input.value){
+      let randomIndex = Math.floor(Math.random() * lowerCaseLetters.length)
+      startingString += lowerCaseLetters[randomIndex]
+      }
+     
+      if (startingString.length < input.value){
+      let randomIndex = Math.floor(Math.random() * numbers.length)
+      startingString += numbers[randomIndex]
+      }
+    
+      if (startingString.length < input.value){
+      let randomIndex = Math.floor(Math.random() * upperCaseLetters.length)
+        startingString += upperCaseLetters[randomIndex]
+      }
+      
+      if (startingString.length < input.value){
+      let randomIndex = Math.floor(Math.random() * symbols.length)
+        startingString += symbols[randomIndex]
+      }
+      
+      const output = document.getElementById('result')
+      output.innerHTML = startingString
+      console.log(startingString)
+    }
+  }
+  document.addEventListener('submit', onSubmit)
+})
